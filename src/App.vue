7a05ae6d4f27a4aeae088b0473967db139ecb9c4
Dennis Metz <info@dmetz.de>
2025-07-24 12:14:01 +0200

    [TASK] Remove unused components, views, and services for code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import BreadCumb from "@/components/BreadCumb.vue";
-</script>
-
 <template>
   <header
     class="flex justify-between items-center dark:bg-slate-500 bg-indigo-500 p-4"
@@ -14,18 +9,39 @@ import BreadCumb from "@/components/BreadCumb.vue";
       width="50"
       height="50"
     />
-
-    <nav class="flex space-x-2 md:space-x-4 text-white">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
   </header>
-  <BreadCumb />
 
   <main class="p-10 flex flex-col space-y-4 md:space-y-8">
-    <RouterView />
+	  <div class="about">
+		  <div class="grid grid-cols-2 gap-2 md:gap-4">
+			  <light-editor v-model="content" />
+			  <light-editor v-model="content" />
+		  </div>
+		  <div class="prose lg:prose-xl" v-html="content"></div>
+	  </div>
   </main>
 </template>
 
-<style>
+<style scoped>
+.about {
+	@apply flex flex-col space-y-4 md:space-y-4;
+}
+
+.is-active {
+	@apply bg-indigo-500 text-white;
+}
+
+.icon {
+	@apply w-4 h-4;
+}
+
+.editor-btn {
+	@apply bg-gray-200 p-2 rounded-md text-gray-700;
+}
 </style>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import LightEditor from "@/plugins/LightEditor.vue";
+const content = ref("");
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 
 import App from "./App.vue";
-import router from "./router";
 import { LightEditorPlugin } from "./plugins";
 // import "./main.css";
 const app = createApp(App);
-app.use(createPinia());
 app.use(LightEditorPlugin);
-app.use(router);
 
 app.mount("#app");
